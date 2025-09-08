2e4ed131741643166ef172bdba11527407c3006e
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-09-08 19:23:18 +0700

    fix: domain

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,9 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cloud.appwrite.io",
+        hostname: "syd.cloud.appwrite.io",
+        port: "",
+        pathname: "/v1/storage/buckets/**",
       },
       {
         protocol: "https",
